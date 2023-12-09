a7ce653ebc0aec1328d740ae956fec079605defa
regulus79 <117475203+regulus79@users.noreply.github.com>
2023-12-08 23:08:58 -0500

    Add LBM to spawn guide

--- mods/regulus_mobs/init.lua
+++ mods/regulus_mobs/init.lua
@@ -7,8 +7,14 @@ dofile(minetest.get_modpath("regulus_mobs").."/boss.lua")
 minetest.register_node("regulus_mobs:spawn_guide1",{
     description="spawn guide1",
     tiles={"regulus_stone1.png"},
-    after_place_node=function(pos,placer)
-        minetest.add_entity(pos+vector.new(0,1,0),"regulus_mobs:guide")
-    end,
     groups={undiggable=1},
+})
+
+minetest.register_lbm({
+    label="spawn guide",
+    name="regulus_mobs:spawn_guide",
+    nodenames={"regulus_mobs:spawn_guide1"},
+    action=function(pos,node,dtime_s)
+        minetest.add_entity(pos+vector.new(0,1,0),"regulus_mobs:guide")
+    end
 })
