0eaec355e68c990240f64ad7502993ad91b7ce12
regulus79 <117475203+regulus79@users.noreply.github.com>
2023-12-08 22:48:01 -0500

    Add node to spawn guide

--- mods/regulus_mobs/init.lua
+++ mods/regulus_mobs/init.lua
@@ -3,3 +3,12 @@
 dofile(minetest.get_modpath("regulus_mobs").."/guide.lua")
 dofile(minetest.get_modpath("regulus_mobs").."/enemy1.lua")
 dofile(minetest.get_modpath("regulus_mobs").."/boss.lua")
+
+minetest.register_node("regulus_mobs:spawn_guide1",{
+    description="spawn guide1",
+    tiles={"regulus_stone1.png"},
+    after_place_node=function(pos,placer)
+        minetest.add_entity(pos+vector.new(0,1,0),"regulus_mobs:guide")
+    end
+    groups={undiggable=true},
+})
