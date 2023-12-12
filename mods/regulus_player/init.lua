3d23f0e57c87da48d744b2e78b7d39d79b0bf8d8
regulus79 <117475203+regulus79@users.noreply.github.com>
2023-12-12 17:23:14 -0500

    Player model and code updates, along with new npc textures

--- mods/regulus_player/init.lua
+++ mods/regulus_player/init.lua
@@ -1,6 +1,6 @@
 regulus_player={}
 
-
+regulus_player.default_player_size=vector.new(1,1,1)
 --should add custom sounds for damage and stuff
 
 
@@ -10,23 +10,27 @@ minetest.register_on_joinplayer(function(player,last_login)
     props.collisionbox={-0.3,0,-0.3,0.3,1.77,0.3}
     props.eye_height=1.625
     props.visual="mesh"
-    props.mesh="regulus_player_model1.obj"
-    props.textures={"regulus_character2.png"},
+    props.visual_size=regulus_player.default_player_size
+    props.mesh="regulus_player_model2.obj"
+    props.textures={"regulus_character_new1.png"},
     minetest.after(0.5,function()
         player:set_properties(props)
     end)
 
     player:get_meta():set_string("powerup","fly")
-    minetest.chat_send_all(dump(player:get_properties().textures))
 
-    --add skybox
-    player:set_sky({
-        base_color="#000000",--todo
-        type="plain",
-        --textures={},--todo
-        clouds=true,
-    })
-    player:override_day_night_ratio(0.3)
+    if player:get_meta():get_int("finished")~=1 then
+        player:set_sky({
+            base_color="#000000",
+            type="plain",
+            clouds=false,
+        })
+        player:set_sun({
+            visible=false,
+            sunrise_visible=false,
+        })
+        player:override_day_night_ratio(0.0)
+    end
 end)
 
 minetest.register_on_newplayer(function(player)
