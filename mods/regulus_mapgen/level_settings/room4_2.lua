33a1e33acb712ced8b74c5a7c32ef10e33532084
regulus79 <117475203+regulus79@users.noreply.github.com>
2023-12-19 21:48:13 -0500

    Make room4_2 easier

--- mods/regulus_mapgen/level_settings/room4_2.lua
+++ mods/regulus_mapgen/level_settings/room4_2.lua
@@ -1,1 +1,1 @@
-return {spawnpoint1={x=2,y=7,z=2},spawnpoint2={x=13,y=15,z=16},spawnpoint3={x=-20,y=47,z=-47},spawnpoint4={x=-20,y=47,z=-47},next2="",next3="",exit="room4",exit_spawnpoint_num=2,extent={x=19,y=18,z=18},level_name="room4_2",next1="room5"}
+return {exit="room4",spawnpoint1={x=2,y=7,z=2},spawnpoint2={x=13,y=15,z=16},next2="",next3="",level_name="room4_2",extent={x=19,y=18,z=18},exit_spawnpoint_num=2,next1="room5",spawnpoint4={x=-20,y=47,z=-47},spawnpoint3={x=-20,y=47,z=-47}}
